264daf039d47a7fcd139112cf1a26490b9ff81c7
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-12-22 09:08:07 +0200

    Import fix

--- payment_paytrail_nets/models/payment_transaction.py
+++ payment_paytrail_nets/models/payment_transaction.py
@@ -9,8 +9,7 @@ from odoo.exceptions import ValidationError
 from odoo.http import request
 
 from odoo.addons.payment import utils as payment_utils
-
-from payment_paytrail_nets.controllers.main import PaytrailController
+from odoo.addons.payment_paytrail_nets.controllers.main import PaytrailController
 
 _logger = logging.getLogger(__name__)
 
